318457c0ca3772829cac870ef7f4df5444771629
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-12-22 10:28:55 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ PATH = _ENV["!plugins_mod_folder_path"].."/"
 
 require("./void_fog")
 
-local diff, spawned_rewards, tp, director
+local diff, spawned_rewards, teleported, tp, director
 void_actor = nil
 local boss_objs = {
     [gm.constants.oBoss1]       = true,
@@ -18,21 +18,21 @@ local boss_objs = {
 }
 
 -- Parameters
-required_waves          = 7
+required_waves          = 20
 radius                  = 1000  -- Radius of safe zone (in pixels)
 void_circle_radius      = 250
 void_color              = Color(0x802E99)
 void_death_time         = 18    -- Time (in seconds) before guaranteed death outside the safe zone
 void_death_time_enemy   = 27
 charge_time             = 20    -- Teleporter charge time (in seconds)
-diff_scale              = 0.12
-damage_tweak            = 0.7   -- Multiplies all enemy damage by this value
-damage_tweak_tweak      = 0.95  -- damage_tweak is multiplied by this value after every stage (heavier damage reduction late game)
-provi_damage_tweak      = 1.25  -- Multiplies Providence's (and his Wurms') damage by this value (applied after damage_tweak)
-health_multiplier       = 0.55
+enemy_buff_base         = 0.6   -- Starting enemy_buff
+enemy_buff_scale        = 0.25  -- enemy_buff linear scaling per stage
+enemy_buff_exp          = 1.02  -- enemy_buff exponential multiplier per stage
 chest_cost_tweak        = 1.8
 
-banned_items            = {
+local damage_tweak_og = damage_tweak
+local ban_applied = false
+local banned_items = {
     ["ror-infusion"]    = false,
     ["ror-umbrella"]    = false
 }
@@ -55,81 +55,21 @@ Initialize(function()
     diff:set_primary_color(void_color)
     diff:set_sound(Resources.sfx_load("klehrik", "simulacrumSfx", PATH.."simulacrum.ogg"))
 
-    diff:set_scaling(
-        diff_scale,
-        0,
-        0
-    )
+    diff:set_scaling(0, 0, 0)
     diff:set_monsoon_or_higher(false)
     diff:set_allow_blight_spawns(false)
 
     -- Add void actor
-    local obj = Object.new("klehrik", "simulacrumVoid", Object.PARENT.actor)
+    local obj = Object.new("klehrik", "simulacrumVoid", Object.PARENT.enemyFlying)
     obj.obj_sprite = Resources.sprite_load("klehrik", "simulacrumVoid", PATH.."simulacrumVoid.png", 1, 20, 23)
-
-    -- diff:onActive(function()
-    --     -- Toggle item ban
-    --     for nsid, v in pairs(banned_items) do
-    --         local item = Item.find(nsid)
-    --         banned_items[nsid] = item:is_loot()
-    --         item:toggle_loot(false)
-    --     end
-    -- end)
-
-    -- diff:onInactive(function()
-    --     log.info("onInactive")
-
-    --     -- Untoggle item ban
-    --     for nsid, v in pairs(banned_items) do
-    --         local item = Item.find(nsid)
-    --         item:toggle_loot(banned_items[nsid])
-    --     end
-    -- end)
 end)
 
 
 Callback.add("onStageStart", "simulacrum-onStageStart", function(self, other, result, args)
     if not diff:is_active() then return end
-    
-    Alarm.create(function()
-        if not tp:exists() then return end
 
-        spawned_rewards = false
-
-        -- Teleport local player to teleporter
-        local player = Player.get_client()
-        player.x, player.ghost_x = tp.x, tp.x
-        player.y, player.ghost_y = tp.y - 12, tp.y - 12
-
-        -- Set camera pan
-        local pan = Instance.find(gm.constants.oPodCameraPan)
-        if pan:exists() then
-            pan.x = tp.x
-            pan.y = tp.y
-            pan.target_y = tp.y
-        end
-        
-        if Net.is_client() then return end
-
-        -- Create void actor
-        void_actor = Object.find("klehrik-simulacrumVoid"):create(-64, -64)
-        void_actor.invincible = 10000000
-
-        if not director:exists() then return end
-
-        -- Replace Divine Teleporters with standard ones until the required number of waves have been cleared
-        local tpe = Instance.find(gm.constants.oTeleporterEpic)
-        if  director.stages_passed < (required_waves - 1)
-        and tpe:exists() then
-            Object.find(gm.constants.oTeleporter):create(tpe.x, tpe.y)
-            tpe:destroy()
-        end
-    end, 1)
-end)
-
-
-Callback.add("preStep", "simulacrum-preStep", function(self, other, result, args)
-    if not diff:is_active() then return end
+    spawned_rewards = false
+    teleported = false
     
     -- Find tp and director
     if not tp:exists() then
@@ -144,6 +84,30 @@ Callback.add("preStep", "simulacrum-preStep", function(self, other, result, args
         director = Instance.find(gm.constants.oDirectorControl)
         if not director:exists() then return end
     end
+
+    -- Manually set enemy_buff
+    director.enemy_buff = (enemy_buff_base + (enemy_buff_scale * director.stages_passed)) * gm.power(enemy_buff_exp, director.stages_passed)
+
+    -- Create void actor
+    if Net.is_client() then return end
+    void_actor = Object.find("klehrik-simulacrumVoid"):create(-64, -64)
+    void_actor.invincible = 10000000
+
+    -- Replace Divine Teleporters with standard ones until the required number of waves have been cleared
+    local tpe = Instance.find(gm.constants.oTeleporterEpic)
+    if  director.stages_passed < (required_waves - 1)
+    and tpe:exists() then
+        tp = Object.find(gm.constants.oTeleporter):create(tpe.x, tpe.y)
+        tp.maxtime = charge_time * 60
+        tpe:destroy()
+    end
+end)
+
+
+Callback.add("preStep", "simulacrum-preStep", function(self, other, result, args)  
+    if not diff:is_active() then return end
+    if not tp:exists() then return end
+    if not director:exists() then return end
 
     -- Spawn rewards on wave completion
     if (not spawned_rewards)
@@ -163,7 +127,10 @@ Callback.add("preStep", "simulacrum-preStep", function(self, other, result, args
         while #contents < 3 do
             local pos = gm.irandom_range(1, #items)
             local item = items[pos]
-            table.insert(contents, item)
+            if  item:is_unlocked()
+            and item:is_loot() then
+                table.insert(contents, item)
+            end
             table.remove(items, pos)
         end
 
@@ -177,11 +144,31 @@ Callback.add("preStep", "simulacrum-preStep", function(self, other, result, args
         end
 
         -- Lower "boss_spawn_points"
-        -- The director gains 700 every time the teleporter is hit
+        -- The director gains 700 every time the teleporter is hit; this will effectively halve gain
         director.boss_spawn_points = director.boss_spawn_points - 350
+    end
+end)
 
-        -- Damage tweak tweak
-        damage_tweak = damage_tweak * damage_tweak_tweak
+
+Callback.add("postStep", "simulacrum-postStep", function(self, other, result, args)
+    if teleported then return end
+    if not tp:exists() then return end
+    
+    teleported = true
+
+    -- Teleport players to teleporter
+    local players = Instance.find_all(gm.constants.oP)
+    for _, player in ipairs(players) do
+        player.x, player.ghost_x = tp.x, tp.x
+        player.y, player.ghost_y = tp.y - 12, tp.y - 12
+    end
+
+    -- Set camera pan
+    local pan = Instance.find(gm.constants.oPodCameraPan)
+    if pan:exists() then
+        pan.x = tp.x
+        pan.y = tp.y
+        pan.target_y = tp.y
     end
 end)
 
@@ -219,42 +206,46 @@ Actor:onPreStep("simulacrum-onPreStep", function(actor)
 end)
 
 
-Actor:onPostStatRecalc("simulacrum-onPostStatRecalc", function(actor)
-    if not diff:is_active() then return end
-    if actor.team ~= 2 then return end
-
-    -- Reduce enemy attack damage and health
-    actor.damage = actor.damage * damage_tweak
-    actor.maxhp = actor.maxhp * health_multiplier
-    actor.maxbarrier = actor.maxbarrier * health_multiplier
-    actor.hp = math.min(actor.hp, actor.maxhp)
-
-    -- Manually set oHUD boss maxhp (??)
-    local hud = Instance.find(gm.constants.oHUD)
-    if hud:exists() and hud.boss_party_active then
-        hud.boss_party_active.hp_total_max = actor.maxhp
-    end
-
-    -- Provi fight damage multiplier
-    if boss_objs[actor.object_index] then
-        actor.damage = actor.damage * provi_damage_tweak
-    end
-end)
-
-
 gm.pre_code_execute("gml_Object_oDirectorControl_Alarm_1", function(self, other)
-    -- Prevent enemies from spawning before the teleporter is hit, as well as on the ship
     if not diff:is_active() then return end
     if not tp:exists() then return end
     if tp.time > 0 then return end
-
+    
+    -- Prevent enemies from spawning before the teleporter is hit, as well as on the ship
     self:alarm_set(1, 60)
     return false
 end)
 
 
 gm.post_script_hook(gm.constants.cost_get_base_gold_price_scale, function(self, other, result, args)
-    -- Scale up natural interactable costs
     if not diff:is_active() then return end
+
+    -- Scale up natural interactable costs
     result.value = result.value * chest_cost_tweak
+end)
+
+
+gm.post_script_hook(gm.constants.run_create, function(self, other, result, args)
+    if not diff:is_active() then return end
+    if ban_applied then return end
+    ban_applied = true
+
+    -- Toggle item ban
+    for nsid, v in pairs(banned_items) do
+        local item = Item.find(nsid)
+        banned_items[nsid] = item:is_loot()
+        item:toggle_loot(false)
+    end
+end)
+
+
+gm.post_script_hook(gm.constants.run_destroy, function(self, other, result, args)
+    if not ban_applied then return end
+    ban_applied = false
+
+    -- Untoggle item ban
+    for nsid, v in pairs(banned_items) do
+        local item = Item.find(nsid)
+        item:toggle_loot(banned_items[nsid])
+    end
 end)
